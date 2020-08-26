733f0e31ffa30e1bde48bf83b0e919281beae4f6
Joep <joep@yourticketprovider.nl>
2020-08-26 11:34:53 +0200

    add WebForms proejct with EndWith extension

---
+++ Geevers.Infrastructure.WebForms/HttpResponseExtensions.cs
@@ -0,0 +1,27 @@
+﻿namespace Geevers.Infrastructure.WebForms
+{
+    using System.Net;
+    using System.Threading;
+    using System.Web;
+
+    public static class HttpResponseExtensions
+    {
+        public static void EndWith(this HttpResponse response, HttpStatusCode status, bool swallowThreadAbortException = true)
+        {
+            response.Clear();
+            response.StatusCode = (int)status;
+            
+            try
+            {
+                response.End();
+            }
+            catch (ThreadAbortException)
+            {
+                if (false == swallowThreadAbortException)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
