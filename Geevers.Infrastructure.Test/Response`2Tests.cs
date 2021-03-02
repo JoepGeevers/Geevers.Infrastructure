27cf3f876f36aff25b93d19338ce5f71a22423ca
Joep <joep@yourticketprovider.nl>
2021-03-02 22:25:33 +0100

    Add Response`1 with tests

---
+++ Geevers.Infrastructure.Test/Response`2Tests.cs
@@ -0,0 +1,83 @@
+﻿namespace Geevers.Infrastructure.Test
+{
+    using System;
+    using System.Drawing;
+    using System.Linq;
+    using System.Net;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Response_2Tests
+    {
+        [TestMethod]
+        public void DefaultResponse_HasStatusNotImplemented_AndDefaultResult()
+        {
+            // arrange
+            Response<Point, Color> response = default;
+
+            // act
+            // assert
+            Assert.AreEqual(HttpStatusCode.NotImplemented, response.Status);
+            Assert.AreEqual(default, response.Result);
+            Assert.AreEqual(default, response.Error);
+        }
+
+        [TestMethod]
+        public void WhenImplicitlyCastingTypeToResponse_ResponseStatusIsOkAndErrorIsNull()
+        {
+            // arrange
+            // act
+            Response<Point, Color> response = new Point(123);
+
+            // assert
+            Assert.AreEqual(HttpStatusCode.OK, response.Status);
+            Assert.AreEqual(new Point(123), response.Result);
+            Assert.AreEqual(default, response.Error);
+        }
+
+        [TestMethod]
+        public void ImplicitlyCastingStatusOtherThenOkResponse_ResponseGetsThatStatus()
+        {
+            // arrange
+            var statii = Enum.GetValues(typeof(HttpStatusCode))
+                .Cast<HttpStatusCode>()
+                .Where(s => s != HttpStatusCode.OK);
+
+            // act
+            var responses = statii
+                .Select(s => (Status: s, Response: (Response<Point, Color>)s));
+
+            // assert
+            Assert.IsTrue(responses.All(p => p.Response.Status == p.Status));
+            Assert.IsTrue(responses.All(p => p.Response.Result == default));
+            Assert.IsTrue(responses.All(p => p.Response.Error == default));
+        }
+
+        [TestMethod]
+        public void ResponseCanBeCreatedAsTupleWithResult()
+        {
+            // arrange
+            Response<Point, Color> response = (HttpStatusCode.Accepted, new Point(123));
+
+            // act
+            // assert
+            Assert.AreEqual(HttpStatusCode.Accepted, response.Status);
+            Assert.AreEqual(new Point(123), response.Result);
+            Assert.AreEqual(default, response.Error);
+        }
+
+        [TestMethod]
+        public void ResponseCanBeCreatedAsTupleWithError()
+        {
+            // arrange
+            Response<Point, Color> response = (HttpStatusCode.RequestEntityTooLarge, Color.RosyBrown);
+
+            // act
+            // assert
+            Assert.AreEqual(HttpStatusCode.RequestEntityTooLarge, response.Status);
+            Assert.AreEqual(default, response.Result);
+            Assert.AreEqual(Color.RosyBrown, response.Error);
+        }
+    }
+}
---
+++ Geevers.Infrastructure/Response`2.cs
@@ -0,0 +1,108 @@
+﻿namespace Geevers.Infrastructure
+{
+    using System;
+    using System.Diagnostics;
+    using System.Net;
+
+    [DebuggerDisplay("{DebuggerDisplay}")]
+    public struct Response<TResult, TError>
+    {
+        private HttpStatusCode? status;
+
+        public HttpStatusCode Status
+        {
+            get
+            {
+                return this.status ?? HttpStatusCode.NotImplemented;
+            }
+            internal set
+            {
+                this.status = value;
+            }
+        }
+
+        private string DebuggerDisplay
+        {
+            get
+            {
+                return this.Status.IsSuccessStatusCode()
+                    ? $"{this.Status}: {this.Result}"
+                    : $"{this.Status}: {this.Error}";
+            }
+        }
+
+        public TResult Result { get; internal set; }
+        public TError Error { get; internal set; }
+
+        public bool IsSuccessStatusCode
+        {
+            get
+            {
+                return this.Status.IsSuccessStatusCode();
+            }
+        }
+
+        public Response(TResult result)
+        {
+            this.status = HttpStatusCode.OK;
+            this.Result = result;
+            this.Error = default;
+        }
+
+        public Response(HttpStatusCode status)
+        {
+            if (status == HttpStatusCode.OK)
+            {
+                throw new InvalidOperationException("You should not need to construct a Response<TResult> with HttpStatusCode.OK. Did you mean NoContent? If not, please either just return the result, or a `new OK(T)`");
+            }
+
+            this.status = status;
+            this.Result = default;
+            this.Error = default;
+        }
+
+        internal Response(HttpStatusCode status, TResult result)
+        {
+            if (false == status.IsSuccessStatusCode())
+            {
+                throw new InvalidOperationException("You cannot construct a Response with an unsuccessfull statuscode and a result");
+            }
+
+            this.status = status;
+            this.Result = result;
+            this.Error = default;
+        }
+
+        internal Response(HttpStatusCode status, TError error)
+        {
+            if (status.IsSuccessStatusCode())
+            {
+                throw new InvalidOperationException("You cannot construct a Response with a successfull statuscode and an error");
+            }
+
+            this.status = status;
+            this.Result = default;
+            this.Error = error;
+        }
+
+        public static implicit operator Response<TResult, TError>(TResult result)
+        {
+            return new Response<TResult, TError>(result);
+        }
+
+        public static implicit operator Response<TResult, TError>(HttpStatusCode status)
+        {
+            return new Response<TResult, TError>(status);
+        }
+
+        public static implicit operator Response<TResult, TError>((HttpStatusCode status, TResult result) response)
+        {
+            return new Response<TResult, TError>(response.status, response.result);
+        }
+
+        public static implicit operator Response<TResult, TError>((HttpStatusCode status, TError error) response)
+        {
+            return new Response<TResult, TError>(response.status, response.error);
+        }
+    }
+}
