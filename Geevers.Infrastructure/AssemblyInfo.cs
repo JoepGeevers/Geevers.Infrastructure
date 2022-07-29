194adbe86580e5fed3656519d41737a569f392e5
Joep Geevers <joepgeevers@gmail.com>
2022-07-29 23:09:47 +0200

    v2.1.0

---
+++ Geevers.Infrastructure/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+
+[assembly: AssemblyCompany("Geevers.Infrastructure")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyVersion("2.1.0")]
+[assembly: AssemblyFileVersion("2.1.0")]
+[assembly: AssemblyInformationalVersion("2.1.0")]
+[assembly: AssemblyProduct("Geevers.Infrastructure")]
+[assembly: AssemblyTitle("Geevers.Infrastructure")]
