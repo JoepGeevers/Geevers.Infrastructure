a240618d10db88309d7700902323fd4caffad90f
Joep Geevers <joepgeevers@gmail.com>
2022-08-25 20:29:25 +0200

    add Id<T> with constructor

---
+++ Geevers.Infrastructure.Test/Id`2.ctor.test.cs
@@ -0,0 +1,114 @@
+﻿namespace Geevers.Infrastructure.Test
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Id_2_Constructor_Test
+    {
+        [TestMethod]
+        public void BoxedValueIsRemembered()
+        {
+            // act
+            var id = new Id<Id_2_Constructor_Test, int>(123);
+
+            // assert
+            Assert.AreEqual(123, id.Value);
+        }
+
+        [TestMethod]
+        public void DefaultKeyIntIsNotAllowed()
+        {
+            // arrange
+            var caught = default(ArgumentException);
+
+            // act
+            try
+            {
+                var id = new Id<Id_2_Constructor_Test, int>(0);
+            }
+            catch (ArgumentException e)
+            {
+                caught = e;
+            }
+
+            // assert
+            Assert.IsNotNull(caught);
+            Assert.AreEqual("Id<T> cannot be null, default or empty", caught.Message);
+        }
+
+        [TestMethod]
+        public void DefaultKeyGuidIsNotAllowed()
+        {
+            // arrange
+            var caught = default(ArgumentException);
+
+            // act
+            try
+            {
+                var id = new Id<Id_2_Constructor_Test, Guid>(Guid.Empty);
+            }
+            catch (ArgumentException e)
+            {
+                caught = e;                                                                                                                                                                                                                 
+            }
+
+            // assert
+            Assert.IsNotNull(caught);
+            Assert.AreEqual("Id<T> cannot be null, default or empty", caught.Message);
+        }
+
+        [TestMethod]
+        public void DefaultKeyStringIsNotAllowed()
+        {
+            // arrange
+            var caught = default(ArgumentException);
+
+            // act
+            try
+            {
+                var id = new Id<Id_2_Constructor_Test, string>(null);
+            }
+            catch (ArgumentException e)
+            {
+                caught = e;
+            }
+
+            // assert
+            Assert.IsNotNull(caught);
+            Assert.AreEqual("Id<T> cannot be null, default or empty", caught.Message);
+        }
+
+        [TestMethod]
+        public void EmptyKeyStringIsNotAllowed()
+        {
+            // arrange
+            var caught = default(ArgumentException);
+
+            // act
+            try
+            {
+                var id = new Id<Id_2_Constructor_Test, string>("");
+            }
+            catch (ArgumentException e)
+            {
+                caught = e;
+            }
+
+            // assert
+            Assert.IsNotNull(caught);
+            Assert.AreEqual("Id<T> cannot be null, default or empty", caught.Message);
+        }
+
+        [TestMethod]
+        public void NonEmptyKeyStringWorks()
+        {
+            // act
+            var id = new Id<Id_2_Constructor_Test, string>("hi");
+
+            // assert
+            Assert.AreEqual("hi", id.Value);
+        }
+    }
+}
---
+++ Geevers.Infrastructure/Id`2.cs
@@ -0,0 +1,27 @@
+﻿namespace Geevers.Infrastructure
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Id<TEntity, TKey>
+    {
+        public readonly TKey Value;
+
+        public Id(TKey value)
+        {
+            if (this.IsNullDefaultOrEmpty(value))
+            {
+                throw new ArgumentException("Id<T> cannot be null, default or empty");
+            }
+
+            this.Value = value;
+        }
+
+        private bool IsNullDefaultOrEmpty(TKey value) =>
+            EqualityComparer<TKey>
+                .Default
+                .Equals(value, default) //stackoverflow.com/a/864860 ~ "Wow, how delightfully obscure! This is definitely the way to go though, kudos. – Nick Farina"
+            ||
+                value as string == "";
+    }
+}
