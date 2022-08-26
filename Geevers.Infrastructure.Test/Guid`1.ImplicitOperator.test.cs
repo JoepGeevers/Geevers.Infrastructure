2e94e56d5a33ee13a0b13445bead81a20d26cb9d
Joep Geevers <joepgeevers@gmail.com>
2022-08-26 09:57:27 +0200

    add Guid<T> with implicit operator test

---
+++ Geevers.Infrastructure.Test/Guid`1.ImplicitOperator.test.cs
@@ -0,0 +1,38 @@
+﻿namespace Geevers.Infrastructure.Test
+{
+    using System;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class Guid_1_ImplicitOperator_Test
+    {
+        [TestMethod]
+        public void IdIsEqualToValueWithImplicitOperator()
+        {
+            // arrange
+            var guid = Guid.NewGuid();
+
+            var one = new Guid<Guid_1_ImplicitOperator_Test>(guid);
+
+            // assert
+            Assert.AreEqual(guid, one);
+            Assert.IsTrue(one.Equals(guid));
+            Assert.IsTrue(one == guid);
+            Assert.IsFalse(one != guid);
+        }
+
+        [TestMethod]
+        public void IdIsNotEqualToValueWithImplicitOperator()
+        {
+            // arrange
+            var one = new Guid<Guid_1_ImplicitOperator_Test>(Guid.NewGuid());
+
+            // assert
+            Assert.AreNotEqual(Guid.NewGuid(), one);
+            Assert.IsFalse(one.Equals(Guid.NewGuid()));
+            Assert.IsFalse(one == Guid.NewGuid());
+            Assert.IsTrue(one != Guid.NewGuid());
+        }
+    }
+}
---
+++ Geevers.Infrastructure/Guid`1.cs
@@ -0,0 +1,13 @@
+﻿namespace Geevers.Infrastructure
+{
+    using System;
+
+    public class Guid<TEntity> : Id<TEntity, Guid>
+    {
+        public Guid(Guid value) : base(value)
+        {
+        }
+
+        public static implicit operator Guid<TEntity>(Guid value) => new Guid<TEntity>(value);
+    }
+}
