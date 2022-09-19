7f31b3d90d2ccbb575cacb8fc09c367b2f667d22
Joep Geevers <joepgeevers@gmail.com>
2022-09-19 14:36:21 +0200

    v3.1.3

--- Geevers.Infrastructure/AssemblyInfo.cs
+++ Geevers.Infrastructure/AssemblyInfo.cs
@@ -3,8 +3,8 @@
 
 [assembly: AssemblyCompany("Geevers.Infrastructure")]
 [assembly: AssemblyConfiguration("Debug")]
-[assembly: AssemblyVersion("3.1.2")]
-[assembly: AssemblyFileVersion("3.1.2")]
-[assembly: AssemblyInformationalVersion("3.1.2")]
+[assembly: AssemblyVersion("3.1.3")]
+[assembly: AssemblyFileVersion("3.1.3")]
+[assembly: AssemblyInformationalVersion("3.1.3")]
 [assembly: AssemblyProduct("Geevers.Infrastructure")]
 [assembly: AssemblyTitle("Geevers.Infrastructure")]
